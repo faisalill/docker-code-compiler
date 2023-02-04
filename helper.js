b4e6e956b7719544aa5d5510cf6f5cd0fdc3193f
faisalill <mfaisal.ilyas.d@gmail.com>
2023-02-04 16:41:03 +0530

    done until rust completely

--- helper.js
+++ helper.js
@@ -1,1 +1,25 @@
+var code= `use std::io;
+use std::{i32};
+ 
+fn main() {
+         
+ 
+          println!("Input First number ? ");
+ 
+          let mut var1 = String::new();
+ 
+          io::stdin().read_line(&mut var1).expect("Unable to read entered data");
+ 
+       
+ 
+          println!("Input second number ? ");
+          let mut var2 = String::new();
+          io::stdin().read_line(&mut var2).expect("Unable to read entered data");
+
+          let a: i32 = var1.trim().parse().ok().expect("Program only processes numbers, Enter number");
+          let b: i32 = var2.trim().parse().ok().expect("Program only processes numbers, Enter number");
+          println!("The sum of a &  b is : {}", a + b);
+ 
+}`
 const formattedCode = code.replace(/\n/g, '\\n').replace(/\"/g, '\\"');
+console.log(formattedCode);
--- routes/route.mjs
+++ routes/route.mjs
@@ -170,7 +170,61 @@ const cExecute = () => {
 }
 
 const rustExecute = () => {
-  console.log('Rust program executed');
+  fs.writeFile('./programs/rustProgram.rs', code, (err) => {
+    if (err){
+      console.log(err);
+    }
+    else{
+  //executes the command to compile the file and give program.exe as output
+  exec('rustc ./programs/rustProgram.rs -o ./output/rustProgram')
+  //watches the current directory for any changes 
+   const cWatcher = fs.watch('./output', (eventType, filename) => {
+     // if the file that is changed is program.exe then it will execute the program
+       if (filename === 'rustProgram') {
+         if(programRunCount === 0){
+          setTimeout(() => {
+            //spawns a child process to execute the program
+            const program = spawn('./output/rustProgram');
+            //listens for data from the child process
+            program.stdout.on('data', (data) => {
+              console.log(`stdout: ${data}`);
+            });
+            //listens for errors from the child process
+            program.stderr.on('data', (data) => {
+              console.log(`stderr: ${data}`);
+            });
+             //writes data to the child process
+             if(input !== ''){
+              program.stdin.write(input);
+             program.stdin.end();
+              }
+            //listens for errors from the child process
+            program.on('error', (error) => {
+              console.log(`error: ${error}`);
+            });
+            //listens for the child process to exit
+            program.on('exit', (code) => {
+              if (code === 0) {
+                console.log(`Exited with code: ${code}`);
+                // resets the programRunCount to 0 so that the program can be executed again
+                programRunCount = 0;
+                // resets the input to an empty string so that the input can be taken again
+                input = '';
+                //closes the watcher
+                cWatcher.close();
+                //removes the Cprogram.exe file
+                removeFile('./output/rustProgram');
+                //removes the Cprogram.c file
+                removeFile('./programs/rustProgram.rs');
+              }
+            });
+          }, 300);
+           programRunCount++;
+         }
+       }
+   });
+    }
+  });
 }
 
 router.get('/', (req, res) => {
